3970f6b1beeae7a8ebd49110bcb01e176607cb6d
Pedro Alves <3pedru@gmail.com>
2017-06-19 22:23:40 -0300

    removido comentarios

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-// const http = require('http');
-//
-// const server = http.createServer(function(req, res){
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Vai 123');
-// });
-//
-// server.listen(3000,() => {
-//   console.log('oba!!! deu tudo certo');
-// });
-
-const express = require('express')
-const app = express()
+const express = require('express'),
+  app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use('/views', express.static('views/assets'))
 
 app.get('/', function(req, res) {
-  // res.send('<h1>tudo certo por enquanto!</h1>')
   res.render('index')
 })
 
 app.get('/form', function(req, res) {
-  // res.send('aqui vamos cadastrar')
   res.render('formulario')
 })
 
 app.get('/lista', function(req, res) {
-  // res.send('vamos listar os dados aqui')
   res.render('lista')
 })
 
