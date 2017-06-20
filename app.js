02e7ffec1d5000ea5149d2379a34e2daf0552cf1
Pedro Alves <3pedru@gmail.com>
2017-06-19 22:22:57 -0300

    faltou 1 linha

--- app.js
+++ app.js
@@ -13,8 +13,9 @@
 const express = require('express')
 const app = express()
 
-app.set('views', './views')
 app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use('/views', express.static('views/assets'))
 
 app.get('/', function(req, res) {
   // res.send('<h1>tudo certo por enquanto!</h1>')
@@ -32,7 +33,11 @@ app.get('/lista', function(req, res) {
 })
 
 app.get('/listaCarregada', function(req, res) {
-  res.status(200).json({a:1,b:2,c:3})
+  res.status(200).json({
+    a: 1,
+    b: 2,
+    c: 3
+  })
 })
 
 app.listen(process.env.PORT || 3000, function() {
