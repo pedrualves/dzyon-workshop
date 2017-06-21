1b3c588479504ead3b8b2be49afb6e5c03a49791
pedrualves <3pedru@gmail.com>
2017-06-21 18:44:31 -0300

    configurado body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   app = express(),
-  mongoose = require('mongoose')
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -8,6 +9,10 @@ app.use('/views', express.static('views/assets'))
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workshop')
 mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.get('/', function(req, res) {
   res.render('index')
@@ -27,6 +32,11 @@ app.get('/listaCarregada', function(req, res) {
     b: 2,
     c: 3
   })
+})
+
+app.post('/registrar', function(req, res) {
+  console.log(req.body);
+  res.render('formulario')
 })
 
 app.listen(process.env.PORT || 3000, function() {
